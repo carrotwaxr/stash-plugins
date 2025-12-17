dafee7bdba9af30245f44517e37417f04281ee1e
carrotwaxr <98351562+carrotwaxr@users.noreply.github.com>
2025-12-17 08:03:29 -0800

    feat(tagManager): add sitemap icon for hierarchy view

--- plugins/tagManager/tag-manager.js
+++ plugins/tagManager/tag-manager.js
@@ -1046,6 +1046,28 @@
   }
 
   /**
+   * Create the Tag Hierarchy nav button SVG icon
+   * Uses a sitemap icon to represent hierarchy/tree view
+   */
+  function createHierarchyIcon() {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('viewBox', '0 0 576 512');
+    svg.setAttribute('class', 'svg-inline--fa fa-icon');
+    svg.setAttribute('aria-hidden', 'true');
+    svg.setAttribute('focusable', 'false');
+    svg.style.width = '1em';
+    svg.style.height = '1em';
+
+    // FontAwesome "sitemap" icon path
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('fill', 'currentColor');
+    path.setAttribute('d', 'M208 80c0-26.5 21.5-48 48-48h64c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48h-8v40H464c30.9 0 56 25.1 56 56v32h8c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48h-64c-26.5 0-48-21.5-48-48v-64c0-26.5 21.5-48 48-48h8v-32c0-4.4-3.6-8-8-8H312v40h8c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48h-64c-26.5 0-48-21.5-48-48v-64c0-26.5 21.5-48 48-48h8v-40H112c-4.4 0-8 3.6-8 8v32h8c26.5 0 48 21.5 48 48v64c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48v-64c0-26.5 21.5-48 48-48h8v-32c0-30.9 25.1-56 56-56h152v-40h-8c-26.5 0-48-21.5-48-48V80z');
+    svg.appendChild(path);
+
+    return svg;
+  }
+
+  /**
    * Inject Tag Manager button into Tags list page toolbar
    */
   function injectTagManagerButton() {
